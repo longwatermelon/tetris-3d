799146881a5fe07bef42618fa3bff2d03d0a11a6
longwatermelon <longwatermelon1@gmail.com>
2022-11-19 14:39:59 -0700

    move to docs

--- docs/index.js
+++ docs/index.js
@@ -1029,7 +1029,7 @@ function createExportWrapper(name, fixedasm) {
 }
 
 var wasmBinaryFile;
-  wasmBinaryFile = 'out.wasm';
+  wasmBinaryFile = 'index.wasm';
   if (!isDataURI(wasmBinaryFile)) {
     wasmBinaryFile = locateFile(wasmBinaryFile);
   }
